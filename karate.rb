c70e01da6dad796cb1daad871fc199407e9fe563
Nestor Castillo <nestor.castillo@izea.com>
2014-07-16 21:56:16 -0400

    dojo progress

--- karate.rb
+++ karate.rb
@@ -1,3 +1,24 @@
 class Karate
+  attr_reader :sorted_array
+
+  def initialize(array)
+    @sorted_array = array.sort
+  end
+
+  def chop(guess)
+    middle_index = middlize(@sorted_array)
+    if @sorted_array[middle_index] == guess
+      return middle_index
+    end
+
+    if guess > @sorted_array[middle_index]
+      @modify_sorted_array = @sorted_array[(middle_index+1)..-1]
+      # stopped here
+    end
+  end
+
+  def middlize(array)
+    (array.length - 1) / 2
+  end
 
 end
--- karate_spec.rb
+++ karate_spec.rb
@@ -1,5 +1,30 @@
 require './karate.rb'
 
 describe Karate do
+  describe :initialize do
+    it "should have a sorted array" do
+      expect(Karate.new([5,4,3,2,1]).sorted_array).to eq([1,2,3,4,5])
+    end
+  end
+
+  describe :chop do
+    it "should return the index if it is the middle" do
+      bl = Karate.new([1,2,3,4,5])
+      expect(bl.chop(3)).to eq(2)
+    end
+  end
+
+  describe :middlize do
+    it "should find middle of the array" do
+      judo = Karate.new([5,4,3,2,1])
+      expect(judo.middlize(judo.sorted_array)).to eq(2)
+    end
+
+    it "should find the number of the first half" do
+      judo = Karate.new([5,4,3,2,1])
+      expect(judo.chop(1)).to eq(4)
+    end
+  end
+
 
 end
